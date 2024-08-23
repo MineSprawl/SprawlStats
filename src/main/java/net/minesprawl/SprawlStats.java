6c71d01f546a66d6e1ed61bbffc6829e5a25d673
Crackle2K <crackleontwitch@gmail.com>
2024-08-23 15:12:08 -0400

    Reformatted commit structure

---
+++ src/main/java/net/minesprawl/SprawlStats.java
@@ -0,0 +1,16 @@
+package net.minesprawl;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SprawlStats implements ModInitializer {
+	public static final String MOD_ID = "sprawlstats";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
