fe5a6af1cd4b6bf794410eba9ec8e788fad76b22
Crackle2K <crackleontwitch@gmail.com>
2024-08-25 14:08:54 -0400

    Refactored SprawlStatsClient

---
+++ src/main/java/net/minesprawl/SprawlStatsClient.java
@@ -0,0 +1,11 @@
+package net.minesprawl;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SprawlStatsClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
