bdac6894e082b0f84edfca1e23852a3899a0c677
Crackle2K <crackleontwitch@gmail.com>
2024-08-28 17:59:19 -0400

    Added registerPackets()

--- src/main/java/net/minesprawl/NetworkHandler.java
+++ src/main/java/net/minesprawl/NetworkHandler.java
@@ -4,5 +4,9 @@ import net.minecraft.util.Identifier;
 
 public class NetworkHandler {
     public static final Identifier SPRAWLSTATS_STATUS_PACKET_ID = Identifier.of("sprawlstats", "sprawlstats_status");
+
+    public static void registerPackets() {
+
+    }
 }
 
