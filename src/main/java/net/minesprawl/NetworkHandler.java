7a3f42b6d26b664a6a8424d1c2736b3eb1d4ae74
Crackle2K <crackleontwitch@gmail.com>
2024-08-28 17:58:33 -0400

    Added NetworkHandler.java

---
+++ src/main/java/net/minesprawl/NetworkHandler.java
@@ -0,0 +1,8 @@
+package net.minesprawl;
+
+import net.minecraft.util.Identifier;
+
+public class NetworkHandler {
+    public static final Identifier SPRAWLSTATS_STATUS_PACKET_ID = Identifier.of("sprawlstats", "sprawlstats_status");
+}
+
