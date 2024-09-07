6586575fd9f3d14d5c67d7e192620dc92c0b6abb
Crackle2K <crackleontwitch@gmail.com>
2024-09-07 18:22:26 -0400

    Deleted workflow

--- src/main/java/net/minesprawl/SprawlStatsClient.java
+++ src/main/java/net/minesprawl/SprawlStatsClient.java
@@ -1,11 +1,17 @@
 package net.minesprawl;
 
+import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.network.PacketByteBuf;
 
 public class SprawlStatsClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
+		PacketByteBuf buf = new PacketByteBuf(Unpooled.buffer());
+		buf.writeBoolean(true);
 
+		ClientPlayNetworking.send(NetworkHandler.SPRAWLSTATS_STATUS_PACKET_ID, buf);
 	}
 }
