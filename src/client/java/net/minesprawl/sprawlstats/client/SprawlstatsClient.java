f7d881eea8aafbae1e16fceb21d1115fd19d7976
swissme <87884098+swissme@users.noreply.github.com>
2024-09-16 23:05:17 -0400

    Converted
    + Converted to new structure
    + Fixed MineSprawl Default Server

---
+++ src/client/java/net/minesprawl/sprawlstats/client/SprawlstatsClient.java
@@ -0,0 +1,12 @@
+package net.minesprawl.sprawlstats.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SprawlstatsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
---
+++ src/main/java/net/minesprawl/sprawlstats/Sprawlstats.java
@@ -0,0 +1,10 @@
+package net.minesprawl.sprawlstats;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Sprawlstats implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
